8e6ec8459ea471c074522f10df83d3e688bc2437
Valeriy Emelyanov <emelva@rambler.ru>
2022-11-19 19:40:22 +0300

    Rates

--- rate-service/src/main/java/com/example/rateservice/dataclient/CbClient.java
+++ rate-service/src/main/java/com/example/rateservice/dataclient/CbClient.java
@@ -7,20 +7,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 
-@FeignClient
+@FeignClient(name = "rates",
+        url = "${cb.url}")
 public interface CbClient {
 
-    @GetMapping(value = "https://www.cbr.ru/scripts/XML_daily.asp")
-    String getCurrencyRatesPlan();
-
-    @GetMapping(value = "https://www.cbr.ru/scripts/XML_daily.asp")
+    @GetMapping
     String getCurrencyRatesOnDate(@RequestParam("date_req") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate date);
 
-    @GetMapping(value = "https://www.cbr.ru/scripts/XML_daily.asp")
+    @GetMapping
     String getCurrencyRatesRange(@RequestParam("date_req1") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dateFrom,
                                  @RequestParam("date_req2") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dateTo,
                                  @RequestParam("VAL_NM_RQ") String currencyId);
 
-    @GetMapping(value = "http://www.cbr.ru/scripts/XML_val.asp?d=0")
-    String getCurrencyList();
 }
---
+++ rate-service/src/main/java/com/example/rateservice/grpcService/RateGrpcService.java
@@ -0,0 +1,56 @@
+package com.example.rateservice.grpcService;
+
+import Grpc.Rate;
+import Grpc.RateServiceGrpc;
+import com.example.rateservice.dto.CurrencyRateRequest;
+import com.example.rateservice.dto.RangeCurrencyRateRequest;
+import com.example.rateservice.service.CurrencyRateService;
+import com.google.gson.Gson;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+@RequiredArgsConstructor
+public class RateGrpcService extends RateServiceGrpc.RateServiceImplBase {
+
+    private final CurrencyRateService currencyRateService;
+
+    private final Gson gson = new Gson();
+
+    @Override
+    public void getCurrentCurrencyRate(Rate.getCurrencyRateRequest request,
+                                       StreamObserver<Rate.getCurrencyRateResponse> responseObserver) {
+        Rate.getCurrencyRateResponse response = Rate.getCurrencyRateResponse.newBuilder()
+                .setResponse(gson.toJson(currencyRateService.getCurrentCurrencyRate(
+                        gson.fromJson(request.getRequest(), CurrencyRateRequest.class)
+                )))
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getRangeCurrencyRate(Rate.getRangeCurrencyRateRequest request,
+                                     StreamObserver<Rate.getRangeCurrencyRateResponse> responseObserver) {
+        Rate.getRangeCurrencyRateResponse response = Rate.getRangeCurrencyRateResponse.newBuilder()
+                .setResponse(gson.toJson(currencyRateService.getRangeCurrencyRate(
+                        gson.fromJson(request.getRequest(), RangeCurrencyRateRequest.class)
+                )))
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getCurrencyList(Rate.emptyRequest request, StreamObserver<Rate.getListCurrencyResponse> responseObserver) {
+        Rate.getListCurrencyResponse response = Rate.getListCurrencyResponse.newBuilder()
+                .setResponse(gson.toJson(currencyRateService.getCurrencyList()))
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
