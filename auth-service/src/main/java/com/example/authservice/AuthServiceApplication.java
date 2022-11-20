f1f214bea66a65ef901b64e064b79ed6d3316561
1thomas3the3train7 <mercyfirsov@gmail.com>
2022-11-20 11:00:10 +0400

    yml

--- auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -1,15 +1,22 @@
 package com.example.authservice;
 
+import com.example.authservice.repository.RedisRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class AuthServiceApplication {
-
+public class AuthServiceApplication implements CommandLineRunner {
+	@Autowired
+	private RedisRepository redisRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
